bc36ef05bd75e481cfc5663603455aedbab1be7c
Ritwick Verma <verma1997@gmail.com>
2017-12-10 01:26:08 +0530

    forgot to change local paths. wow. added support for windows. windows sucks.

--- Readme.md
+++ Readme.md
@@ -1,13 +1,14 @@
 ### What the fook is this?
 
-This script downloads a wallpaper of your screen resolution from [Unsplash](https://unsplash.com/) and sets it as your wallpaper. It'll save the downloaded image in pictures folder. You can also select if you wanna play a random song from your music folder in the default music player. The script waits for the internet till eternity if your net's like mine (not working). Oh yes, theres a notification too.  
+This script downloads a wallpaper of your screen resolution from [Unsplash](https://unsplash.com/) and sets it as your wallpaper. It'll save the downloaded image in the folder of this script. The script waits for the internet till eternity if your net's like mine (not working/ too slow to notice it's working). Oh yes, there's a notification too (not in Windows. Windows is shit).  
 #### Few things to remember:
-* Only for **linux**
+* Now it actually works. I think.
+* Only for linux and windows.
 * Needs python2 and tkinter
-* Tested on GNOME, Pantheon. Might not work on Unity.
-* Doesn't work with python3. Maybe I update it in the future so that it does (no I won't, I got no chill)
+* Tested on GNOME de only. I don't think it works elsewhere. In the future maybe?
+* Doesn't work with python3. In the future maybe?.
 * If it's not working, it's okay. It's lame anyway. Just download wallpaper yourself you lazy PoS.
-* Stop hate crime againt asian midget grannies. I'm strictly against it.  
+* Stop hate crime against asian midget grannies. I'm strictly against it #asianmidgetgrannielivesmatter  
 
 #### Working ok
 ![screenshot](https://github.com/RitwickVerma/lamewallpaperchangingpythonscriptforlazypeeps/blob/master/workingok.png "You won't know if this is real or I just downloaded a wallpaper and set it myself.")
--- lamewallpaperchangingpythonscriptforlazypeeps.py
+++ lamewallpaperchangingpythonscriptforlazypeeps.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
 import urllib2
 import urllib
 import os
 import subprocess as sp
 import Tkinter as tk
-import random
+import random, platform, ctypes, sys
 
 def connected():
     while True:
@@ -18,19 +17,22 @@ def createdialog():
 
     def changewall():
         root.quit()
+        SPI_SETDESKWALLPAPER = 20
+        dirurl=sys.path[0]
         screen_width = root.winfo_screenwidth()
         screen_height = root.winfo_screenheight()
         category="?"+categoryoptionsvar.get()
         featured=featuredcheckbuttonvar.get()
         url="https://source.unsplash.com/"+("featured/" if(featured) else "")+str(screen_width)+"x"+str(screen_height)+"/"+ ("" if(category=="?Random") else category)
-        print(url)
-        urllib.urlretrieve(url,"/home/ritwick/Pictures/Wallpapers/background.jpg")
-        os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri /home/ritwick/Pictures/Wallpapers/background.jpg")
-        musicurl="\"/home/ritwick/Music/"+random.choice(os.listdir("/home/ritwick/Music/"))+"\""
-        print(musicurl)
-        if(musiccheckbuttonvar.get()):
-            os.system("xdg-open "+musicurl)
-        sp.call(['notify-send','-t','5000','You are awesome','Wallpaper changed from unsplashed'])
+        print(dirurl)
+        urllib.urlretrieve(url,"background.jpg")
+        if(platform.system().lower().find('windows')==-1):
+            os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri "+dirurl)
+            sp.call(['notify-send','-t','5000','You are awesome','Wallpaper changed from unsplashed'])
+
+        else:
+            ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, dirurl+"\\background.jpg" , 0)
+
 
     categorytext=tk.Label(root,text="Categories")
     categorytext.pack()
@@ -47,11 +49,6 @@ def createdialog():
     featuredcheckbutton=tk.Checkbutton(root,text="Featured",variable=featuredcheckbuttonvar)
     featuredcheckbutton.pack()
     featuredcheckbutton.place(x=10,y=50)
-
-    musiccheckbuttonvar=tk.IntVar()
-    musiccheckbutton=tk.Checkbutton(root,text="Music",variable=musiccheckbuttonvar)
-    musiccheckbutton.pack()
-    musiccheckbutton.place(x=10,y=80)
 
     gobutton=tk.Button(root,text="Go!",command=changewall)
     gobutton.pack()
